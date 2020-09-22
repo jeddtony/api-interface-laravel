a6f9722209b01cd2cc56ec6fca52af86f92ea16e
jeddtony <jedtony.jt@gmail.com>
2020-09-22 21:52:24 +0100

    Add seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
+use App\Models\Staff;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        Department::factory(10)->create();
+        Staff::factory(20)->create();
     }
 }
